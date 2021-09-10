52070d37f55351ece1494e86e948d76ca9377824
Loray21 <loray98@gmail.com>
2021-09-10 17:54:13 -0300

    filtro negativo && redimensionar imagen

--- Entregable/js/filtros.js
+++ Entregable/js/filtros.js
@@ -1,59 +1,63 @@
 var imagen_ejemplo = new Image();
- var ctx = document.getElementById("canvas").getContext("2d");
-   document.getElementById('file').onchange= function(e){
-    let reader = new FileReader();
-    reader.readAsDataURL(e.target.files[0]);
-    reader.onloadend = function (){
-      imagen_ejemplo.src= reader.result;
-      imagen_ejemplo.onload = function(){
+var canvas = document.getElementById("canvas");
+var ctx = canvas.getContext("2d");
+document.getElementById("file").onchange = function (e) {
+  let reader = new FileReader();
+  reader.readAsDataURL(e.target.files[0]);
+  reader.onload = function () {
+    imagen_ejemplo.src = reader.result;
+    redimensionarImagen(imagen_ejemplo, canvas);
+    imagen_ejemplo.onload = function () {
       dibujarImg(imagen_ejemplo);
       var imageData = ctx.getImageData(0, 0, imagen_ejemplo.width, imagen_ejemplo.height);
-      ctx.putImageData(imageData,0,0);
 
-      //recorro ancho y alto de la imagen (iamge data la matriz y voy pintando perro)
-     /*for (var eje_x = 0; eje_x < imageData.width; eje_x ++) {
-       for (var eje_y = 0; eje_y < imageData.height; eje_y ++) {
-         setPixel(imageData,eje_x,eje_y);
-        ; 
-   
-    }
-   
-   }*/
-   
-    }
+      for (var x = 0; x < canvas.width; x++) {
+        for (var y = 0; y < canvas.height; y++) {
+          //gris(imageData, x, y);
+          negativo(imageData, x, y);
+        }
+      }
+    };
+  };
+};
+function dibujarImg(imagen) {
+  ctx.drawImage(imagen, 0, 0, 600, 600);
+}
+
+
+function redimensionarImagen(imagen, canvas) {
+  //cuando la imagen es mas chica q el canvas
+  if (imagen.width < canvas.width && imagen.height < canvas.height) {
+    imagen.width = imagen.width - (imagen.width - canvas.width);
+    imagen.height = imagen.height - (imagen.height - canvas.height);
+  }
+  if (imagen.width > canvas.width && imagen.height > canvas.height) {
+    imagen.width = imagen_ejemplo.width + (imagen_ejemplo.width - canvas.width);
+    imagen.height = imagen_ejemplo.height + (imagen_ejemplo.height - canvas.height);
   }
 }
- /*imagen.onload = function(){
-   dibujarImg(this);
-   var imageData = ctx.getImageData(0, 0, this.width, this.height);
-   //recorro ancho y alto de la imagen (iamge data la matriz y voy pintando perro)
-  for (var eje_x = 0; eje_x < imageData.width; eje_x ++) {
-    for (var eje_y = 0; eje_y < imageData.height; eje_y ++) {
-      setPixel(imageData,eje_x,eje_y);
-     ; 
 
- }
+//calcula el tono de gris y si es mayor que 255/2 pones 255, si no 0 
 
-}
+//variable brillo 
 
- }
-*/
- function dibujarImg(imagen){
-   ctx.drawImage(imagen,0,0,500,450);
- }
-
-
-
-
-  function setPixel(imageData,x,y){
-  
-  index=(x+y*imageData.width)*4//agarro el índice de la matriz // covnieron en matriz
+function gris(imageData, x, y) {
+  index = (x + y * imageData.width) * 4; //agarro el índice de la matriz // covnieron en matriz
   //sacp promedio de los r g b
-  let promedio= (imageData.data[index+0] + imageData.data[index+1] + imageData.data[index+2]) / 3;
-   imageData.data[index+0] = promedio;
-  imageData.data[index+1] = promedio;
-  imageData.data[index+2] = promedio;
+  let promedio = (imageData.data[index + 0] + imageData.data[index + 1] + imageData.data[index + 2]) / 3;
+  imageData.data[index + 0] = promedio;
+  imageData.data[index + 1] = promedio;
+  imageData.data[index + 2] = promedio;
   //coloco la imagen en el canvas
-  ctx.putImageData(imageData,0,0);
+  ctx.putImageData(imageData, 0, 0);
+}
 
-  }
+function negativo(imageData, x, y) {
+  index = (x + y * imageData.width) * 4; //agarro el índice de la matriz // covnieron en matriz
+  //sacp promedio de los r g b
+  imageData.data[index + 0] = 255 - imageData.data[index + 0];
+  imageData.data[index + 1] = 255 - imageData.data[index + 1];
+  imageData.data[index + 2] = 255 - imageData.data[index + 2];
+  //coloco la imagen en el canvas
+  ctx.putImageData(imageData, 0, 0);
+}
