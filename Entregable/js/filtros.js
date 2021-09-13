b85930317c3022d706c015c4d659904d8a001d42
LautaroFrijon <42587900+LautaroFrijon@users.noreply.github.com>
2021-09-13 20:46:28 -0300

    Subo el guardar imagen, tambien uni los dos archivos, el paint y los filtros

--- Entregable/js/filtros.js
+++ Entregable/js/filtros.js
@@ -2,9 +2,10 @@
 var canvas = document.getElementById("canvas");
 var ctx = canvas.getContext("2d");
 ctx.fillStyle = '#FFFFFF';
-ctx.fillRect(0, 0, 800, 800);
+ctx.fillRect(0, 0, canvas.width, canvas.height);
 var imagen = document.getElementById('file').addEventListener('change', cargarImagen, false);
 
+//Eventos
 document.getElementById("binarizacion").addEventListener("click", function () {
   binarizacion();
 });
@@ -20,6 +21,7 @@ document.getElementById("sepia").addEventListener("click", function () {
 document.getElementById("negativo").addEventListener("click", function () {
   negativo();
 });
+
 document.getElementById("saturacion").addEventListener("click", function () {
   saturacion()
 });
@@ -32,11 +34,106 @@ document.getElementById("blur").addEventListener("click", function () {
   blur();
 })
 
+document.getElementById("guardar").addEventListener("click", function(){
+  var link = window.document.createElement('a'),
+        url = canvas.toDataURL(),
+        filename = 'imagen.jpg';
+    link.setAttribute('href', url);
+    link.setAttribute('download', filename);
+    link.style.visibility = 'hidden';
+    window.document.body.appendChild(link);
+    link.click();
+    window.document.body.removeChild(link);
+})
+
+//Parte Paint
+
+//ESTA FUNCION ME DEVUELVE
+//A DONDE ESTA EL CANVAS 
+//CON RESPECTO A LA PANTALLA
+let rect = canvas.getBoundingClientRect();
+let x = 0;
+let y = 0;
+//saber cuando esta dibujando
+let dibujando = false;
+let color = 'black';
+let grosor = 1;
+
+function defcolor(c) {
+    color = c;
+}
+
+function defgrosor(g) {
+    grosor = g;
+}
+
+document.getElementById("goma").addEventListener("click", function () {
+    color = 'white';
+})
+
+//cuando baja y empieza a dibujar
+canvas.addEventListener('mousedown', function (e) {
+    //ressto el clientex - rect parea saber donde esta el canvas dentro de la pantalla
+    x = e.clientX - rect.left;
+    y = e.clientY - rect.top;
+    dibujando = true;
+})
+
+canvas.addEventListener('mousemove', function (e) {
+    if (dibujando === true) {
+        //le paso el punto inicial donde hice click la primer vez(x,y)
+        //y los parametros sigueinte vuelvo a calcular donde estoy en este momento
+        dibujar(x, y, e.clientX - rect.left, e.clientY - rect.top)
+        //actualizo x e y con el punto donde me encuentro
+        x = e.clientX - rect.left;
+        y = e.clientY - rect.top;
+    }
+})
+
+//suelta el click
+canvas.addEventListener('mouseup', function (e) {
+    if (dibujando == true) {
+        //dibujo la linea
+        dibujar(x, y, e.clientX - rect.left, e.clientY - rect.top);
+        //reseto valores
+        x = 0;
+        y = 0;
+        dibujando = false;
+    }
+})
+
+function dibujar(x1, y1, x2, y2) {
+    //indico nueva ruta
+    ctx.beginPath();
+    ctx.strokeStyle = color;
+    ctx.lineWidth = grosor;
+    ctx.moveTo(x1, y1);
+    //crea la linea desde del ultimo punto es decir  (x1,y1) a (x2,y2)
+    ctx.lineTo(x2, y2);
+    ctx.stroke();
+    ctx.closePath();
+}
+
+function borrar() {
+    ctx.beginPath();
+    ctx.strokeStyle = 'white';
+    ctx.lineWidth = grosor;
+    ctx.moveTo(x1, y1);
+    ctx.lineTo(x2, y2);
+    ctx.stroke();
+    ctx.closePath();
+}
+
+document.getElementById('clearCanvas').addEventListener("click", function () {
+    ctx.fillStyle = '#F8F8FF'
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+})
+
+//Cargar imagen desde el disco
 function cargarImagen(e) {
   var reader = new FileReader();
   reader.onload = function (event) {
     var imagen1 = new Image();
-
     imagen1.onload = function () {
       dibujarImg(this);
     }
@@ -46,7 +143,6 @@ function cargarImagen(e) {
 }
 
 function dibujarImg(imagen) {
-
   ctx.drawImage(imagen, 0, 0, 800, 800);
   redimensionarImagen(imagen, canvas)
 
@@ -69,7 +165,7 @@ function redimensionarImagen(imagen, canvas) {
   }
 }
 
-//variable brillo 
+//Filtros. 
 
 //Calculamos el promedio de los colores y si es mayor 255/2, se le pone 255 directamente,
 //en caso contrario, se lo pone en 0.
